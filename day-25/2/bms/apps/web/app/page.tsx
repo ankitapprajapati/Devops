baa2de658931d57023a87025670ce4790fae1ff8
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2025-10-24 00:07:02 +0530

    checking for cicd

--- day-25/2/bms/apps/web/app/page.tsx
+++ day-25/2/bms/apps/web/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <div className={styles.page}>
       <h1>Welcome to BMS Web App</h1>
       <div>
-        <h1>Name</h1> : 
+        <h1>First Name</h1> : 
         {user ?.username}
         <h1>Password</h1> : 
         {user ?.password}
