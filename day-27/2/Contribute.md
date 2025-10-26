1f58affa2b90c0427b6922d92cd9bf0008ddbf13
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2025-10-26 13:04:32 +0530

    fix Prisma client error

--- day-27/2/Contribute.md
+++ day-27/2/Contribute.md
@@ -6,11 +6,18 @@
         -docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres
         -go to neon.tech and get yourself a new db ( kept it ans DATABASE_URL)
     - Change the .env file and update the credential
+    - npx prisma migrate dev
     - npx prisma generate
-    - npx prisma migrate
     - npm run build
     - npm run start
 
 ## Docker installations
+    - Install docker
+    - Start postgress 
+        - docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:54432 postgres
+    - Build the image - `docker build -t user-project .`
+    - Start the image - `docker run -p 3000:3000 user-project`
 
 ## Docker Compose Installations steps
+    - Install docker, docker compose
+    - Run `docker-compose up`
--- day-27/2/src/index.ts
+++ day-27/2/src/index.ts
@@ -1,48 +1,12 @@
-import express from 'express';
-import { userInfo } from 'node:os';
-const app = express();
-const port = 3000;
+import { PrismaClient } from '@prisma/client';
 
-const prismaClient = require('@prisma/client');
+const prisma = new PrismaClient();
 
-app.use(express.json());
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
 
-app.get('/', async (req, res) => {
-  try{
-    const user = await prismaClient.user.findFirst();
-    if(!user){
-      return res.status(404).json({message: 'No user found'});
-    }
-    res.status(200).json({
-      id: user.id,
-      username: user.username,
-      password: user.password
-    });
-  }
-  catch(error){
-    res.status(500).send('Error retrieving user');
-  }
-}); 
-
-app.post('/', async (req, res) => {
-  try{
-    // const username = req.body.username;
-    // const password = req.body.password;
-    const user = await prismaClient.user.create({
-      data: {
-        // username: username,
-        // password: password
-        username : Math.random().toString(36).substring(7),
-        password : Math.random().toString(36).substring(7)
-      }
-    });
-    res.status(201).send('User created successfully');
-  }
-  catch(error){
-    res.status(500).send('Error creating user');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+main()
+  .catch(e => console.error(e))
+  .finally(() => prisma.$disconnect());
