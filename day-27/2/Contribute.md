2bbd120c826a2592af3d0873ee91f94adb715fba
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2025-10-27 00:24:28 +0530

    docker-compose

--- day-27/2/Contribute.md
+++ day-27/2/Contribute.md
@@ -13,9 +13,11 @@
 
 ## Docker installations
     - Install docker
+    - Start network
+        - docker network create user_network
     - Start postgress 
-        - docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:54432 postgres
-    - Build the image - `docker build -t user-project .`
+        - docker run --network user_network -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres
+    - Build the image - `docker build -t user_project .`
     - Start the image - `docker run -p 3000:3000 user-project`
 
 ## Docker Compose Installations steps
--- day-27/2/src/index.ts
+++ day-27/2/src/index.ts
@@ -1,12 +1,37 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client"
+import express from "express"
 
+const app = express()
 const prisma = new PrismaClient();
 
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
-}
+app.get("/", async (req, res) => {
+   
+    const data = await prisma.user.findMany();
+    res.json({
+        data : data
+    })
+})
 
-main()
-  .catch(e => console.error(e))
-  .finally(() => prisma.$disconnect());
+app.post("/", async (req, res) => {
+  try{
+    const user = await prisma.user.create({
+      data:{
+        username: Math.random().toString(36).substring(7),
+        password: Math.random().toString(36).substring(7)}
+      }
+    );
+    res.json({
+      message : "User created successfully",
+      user
+    })
+  }catch(err){
+    res.status(500).json({
+      message : "Something went wrong",
+      error: err
+    })
+  }
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
